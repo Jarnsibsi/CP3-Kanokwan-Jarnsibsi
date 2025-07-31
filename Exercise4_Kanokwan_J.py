b1d73a80e17695f47825975ab05ad4f71a3c450f
jarnsibsi <jarnsibsi.k@gmail.com>
2025-07-31 17:20:18 +0700

    Exercise4

---
+++ Exercise4_Kanokwan_J.py
@@ -0,0 +1,13 @@
+FE = float(input("Foundation English : "))
+GB = float(input("General Business : "))
+IC = float(input("Introduction to Computer Systems: "))
+CP = float(input("Computer Programming: "))
+
+print("--- Your Score ---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",IC)
+print("Computer Programming : ",CP)
+
+
+
