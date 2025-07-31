4baad604da1242b0958c48b983db896235f9b5c3
jarnsibsi <jarnsibsi.k@gmail.com>
2025-07-31 17:26:22 +0700

    Updated Excercise4

--- assigments/Exercise4_Kanokwan_J.py
+++ assigments/Exercise4_Kanokwan_J.py
@@ -1,13 +1,13 @@
-FE = float(input("Foundation English : "))
-GB = float(input("General Business : "))
-IC = float(input("Introduction to Computer Systems: "))
-CP = float(input("Computer Programming: "))
+Foundation_English = float(input("Foundation English : "))
+General_Business = float(input("General Business : "))
+Introduction_to_Computer_Systems = float(input("Introduction to Computer Systems: "))
+Computer_Programming = float(input("Computer Programming: "))
 
 print("--- Your Score ---")
-print("Foundation English : ",FE)
-print("General Business : ",GB)
-print("Introduction to Computer Systems : ",IC)
-print("Computer Programming : ",CP)
+print("Foundation English : ",Foundation_English)
+print("General Business : ",General_Business)
+print("Introduction to Computer Systems : ",Introduction_to_Computer_Systems)
+print("Computer Programming : ",Computer_Programming)
 
 
 
