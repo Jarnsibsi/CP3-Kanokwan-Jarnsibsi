3dc9cc8c396fd46ad2c0ed48611ca3e6c18ee3d9
jarnsibsi <jarnsibsi.k@gmail.com>
2025-08-16 16:08:03 +0700

    Updated Lecture50,53

---
+++ assignments/Lecture50_Kanokwan_J.py
@@ -0,0 +1,16 @@
+def addNumber(x,y):
+    print(x+y)
+    
+def minusNumber(x,y):
+    print(x-y)
+    
+def multiplyNumber(x,y):
+    print(x*y)    
+    
+def devisionNumber(x,y):
+    print (x/y)
+    
+addNumber(10,5)
+minusNumber(10,5)
+multiplyNumber(10,5)
+devisionNumber(10,5)
